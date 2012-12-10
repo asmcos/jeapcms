fba4d9ab169e9f6bd364bcd2e1543dafc15d72b6
peter <asmcos@gmail.com>
2012-12-10 15:25:53 +0800

    modify  display category in index.html by id

--- apps/cms/views.py
+++ apps/cms/views.py
@@ -12,3 +12,7 @@ def index():
 def category_content(id,limit=10):
 	cate_cont = content.filter(content.c.cateid == id).limit(limit)
 	return cate_cont 
+
+def category_get(limit=10):
+	cate_name = category.all().limit(limit)
+	return cate_name 
