278f243f86cf667fe13a8826abbe7e0e48808754
asmcos <asmcos@jeapedu.com>
2012-12-19 07:26:24 +0800

    modify database

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -3,26 +3,26 @@
 from uliweb.orm import *
 
 class siteinfo(Model):
-	name   = Field(CHAR)
-	url    = Field(CHAR)
-	desc   = Field(CHAR)
+	name   = Field(str, max_length=255)
+	url    = Field(str, max_length=255)
+	desc   = Field(str, max_length=1024)
 #	title  = Field(CHAR)
-	temp   = Field(CHAR)
-	logo   = Field(CHAR)
+	temp   = Field(str, max_length=255)
+	logo   = Field(str, max_length=255)
 
 class category(Model):
 	id_order = Field(int)
-	name     = Field(CHAR)
+	name     = Field(str, max_length=255)
 	num      = Field(int)
-	temp     = Field(CHAR)
-	image    = Field(CHAR)
+	temp     = Field(str, max_length=255)
+	image    = Field(str, max_length=255)
 
 class content(Model):
 	id_order = Field(int)
 	cateid   = Field(int)
-	title    = Field(CHAR)
-	content  = Field(CHAR)
-	image    = Field(CHAR)
+	title    = Field(str, max_length=255)
+	content  = Field(TEXT)
+	image    = Field(str, max_length=255)
 
 
 
