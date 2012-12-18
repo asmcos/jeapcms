b4676a77e0517bbcc8226315f2629f43466669b5
peter <asmcos@gmail.com>
2012-12-18 18:34:07 +0800

    add subString

--- apps/cms/views.py
+++ apps/cms/views.py
@@ -23,4 +23,37 @@ def category_show(id):
 	cate        = category.all()
 	cate_info = category.get(category.c.id==id)
 	cate_list = content.filter(content.c.cateid == id)
-	return {'site':site,'cate':cate,'cate_list':cate_list,'cate_info':cate_info}	
+	return {'site':site,'cate':cate,'cate_list':cate_list,'cate_info':cate_info}
+
+
+def subString(string,length):  
+    if length >= len(string):  
+        return string  
+    result = ''  
+    i = 0  
+    p = 0  
+    while True:  
+        ch = ord(string[i])  
+        #1111110x  
+        if ch >= 252:  
+            p = p + 6  
+        #111110xx  
+        elif ch >= 248:  
+            p = p + 5  
+        #11110xxx  
+        elif ch >= 240:  
+            p = p + 4  
+        #1110xxxx  
+        elif ch >= 224:  
+            p = p + 3  
+        #110xxxxx  
+        elif ch >= 192:  
+            p = p + 2  
+        else:  
+            p = p + 1     
+              
+        if p >= length:  
+            break;  
+        else:  
+            i = p  
+        return string[0:i]  	
